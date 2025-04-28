d4959a36b43dbbddc0d16e6b99f587d9475baca7
*CxC6TdYE92 <huukhoa04@gmail.com>
2025-04-28 19:48:00 +0700

    feat: - Set up shadcn/ui - Set up React Router - Added README

--- rt-data-science/eslint.config.js
+++ rt-data-science/eslint.config.js
@@ -3,19 +3,32 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import pathAlias from 'eslint-plugin-import-alias'
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended, 
+      ...tseslint.configs.recommended,
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        project: './tsconfig.json',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true
+        },
+        tsconfigRootDir: '.'
+      },
     },
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'import-alias': pathAlias
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -23,6 +36,26 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      // Add rules for path aliases
+      'import-alias/import-alias': [
+        'error',
+        {
+          relativeDepth: 0,
+          aliases: [
+            { alias: '@', matcher: '^src' }
+          ]
+        }
+      ],
+      // Disable some rules that might interfere with path aliases
+      'no-restricted-imports': 'off',
+      '@typescript-eslint/no-restricted-imports': 'off',
     },
+    settings: {
+      'import/resolver': {
+        typescript: {
+          project: './tsconfig.json'
+        }
+      }
+    }
   },
 )
